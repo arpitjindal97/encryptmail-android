55970c5d03977e4f603fa83ecfe5d54e0cf7f26c
Arpit Agarwal <arpitjindal97@gmail.com>
2018-06-29 22:09:37 +0530

    Initial commit

---
+++ app/src/main/java/com/encryptmail/email/ui/splash/SlashActivity.kt
@@ -0,0 +1,13 @@
+package com.encryptmail.email.ui.splash
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.encryptmail.android.R
+
+class SlashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.splash_activity)
+    }
+}
---
+++ app/src/main/java/com/encryptmail/email/ui/splash/SplashViewmodel.kt
@@ -0,0 +1,7 @@
+package com.encryptmail.email.ui.splash
+
+import android.arch.lifecycle.ViewModel
+
+class SplashViewmodel : ViewModel(){
+
+}
